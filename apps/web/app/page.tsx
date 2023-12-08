64b95e558f0e99c58336f1adef9c852fc7af8b9c
TevinZielke <tevin.zielke@muse-case.com>
2023-12-08 20:43:41 +0100

    change default @repo to @jamphlet

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
+import { Card } from "@jamphlet/ui/card";
+import { Code } from "@jamphlet/ui/code";
 import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+import { Button } from "@jamphlet/ui/button";
 
 function Gradient({
   conic,
